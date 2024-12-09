5d6b1282e325c23463bb583fcee555662e002af6
Lalit Das <lalit.das@celigo.com>
2024-12-09 10:04:50 +0530

    failuer case

---
+++ test/add2.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { add } from "../src/add";
+
+describe("Addition", () => {
+  it("should return the sum of two numbers", () => {
+    expect(add(2, 3)).to.equal(6);
+  });
+});
